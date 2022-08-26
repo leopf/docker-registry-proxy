297a7e475ccc7d4f03a1960ec2d3c67823f86f7d
leopf <l.pfob@3-klicks.de>
2022-08-26 20:55:23 +0200

    basic auth validation

--- src/utils/basic-auth.ts
+++ src/utils/basic-auth.ts
@@ -1,4 +1,8 @@
 export function createBasicAuthHeaderValue(username: string, password: string) {
+    if (username.includes(":") || password.includes(":")) {
+        throw new Error("username and password cannot contain a colon!");
+    }
+
     return `basic ${Buffer.from(
         username +
         ':' +
